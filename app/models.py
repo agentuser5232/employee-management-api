f5aa86c8f805c6070def00ced62466fa2bfc9c1e
Project Genesis <iwtlab29@gmail.com>
2026-07-10 18:44:32 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from .database import Base
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    department = Column(String(100))
+    email = Column(String(150), unique=True)
